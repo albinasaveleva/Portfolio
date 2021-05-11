08bad6a1eacde1d256996d9bd274ebd6fd8a9aa4
albinasaveleva <savelevalbina@gmail.com>
2021-05-11 17:55:02 +0300

    add photography section
    add structure, elements, styles and adaptive

--- js/main.js
+++ js/main.js
@@ -9,3 +9,22 @@ burgerMenuButton.addEventListener('click', function() {
 burgerMenuButtonExit.addEventListener('click', function() {
   burgerMenu.classList.add('burger-menu_inactive');
 })
+
+let tabs = document.querySelector(".photography-section__content");
+let tabButton = document.querySelectorAll(".tab-button");
+let tabContents = document.querySelectorAll(".tab-content");
+
+tabs.addEventListener("click", function(e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    tabButton.forEach(function(btn) {
+      btn.classList.remove("tab-button_active");
+    })
+  }
+  e.target.classList.add("tab-button_active");
+  tabContents.forEach(function(content) {
+    content.classList.remove("tab-content_active");
+  })
+  const elem = document.getElementById(id);
+  elem.classList.add("tab-content_active");
+});
