c7e64579c033b14044e7dab7a4c2e80cd983f00a
albinasaveleva <savelevalbina@gmail.com>
2021-05-14 15:50:04 +0300

    add anchors and up-button

--- js/main.js
+++ js/main.js
@@ -2,29 +2,34 @@ let burgerMenu = document.querySelector("#burger-menu");
 let burgerMenuButton = document.querySelector("#burger-menu__button");
 let burgerMenuButtonExit = document.querySelector("#burger-menu__button-exit");
 
-
-burgerMenuButton.addEventListener('click', function() {
-  burgerMenu.classList.remove('burger-menu_inactive');
-})
-burgerMenuButtonExit.addEventListener('click', function() {
-  burgerMenu.classList.add('burger-menu_inactive');
-})
+burgerMenuButton.addEventListener("click", function () {
+  burgerMenu.classList.remove("burger-menu_inactive");
+});
+burgerMenuButtonExit.addEventListener("click", function () {
+  burgerMenu.classList.add("burger-menu_inactive");
+});
 
 let tabs = document.querySelector(".photography-section__content");
 let tabButton = document.querySelectorAll(".tab-button");
 let tabContents = document.querySelectorAll(".tab-content");
 
-tabs.addEventListener("click", function(e) {
+tabs.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   if (id) {
-    tabButton.forEach(function(btn) {
+    tabButton.forEach(function (btn) {
       btn.classList.remove("tab-button_active");
-    })
+    });
   }
   e.target.classList.add("tab-button_active");
-  tabContents.forEach(function(content) {
+  tabContents.forEach(function (content) {
     content.classList.remove("tab-content_active");
-  })
+  });
   const elem = document.getElementById(id);
   elem.classList.add("tab-content_active");
+});
+
+const upBtn = document.querySelector("#up-button");
+upBtn.addEventListener("click", function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
